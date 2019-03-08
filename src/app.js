20b0b767931cc0c9416c3bcd85722818f47e67ca
Unknown <oliveira_fabio@id.uff.br>
2019-03-07 22:42:59 -0300

    operacao crud em node.js

---
+++ src/app.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const app = express();
+//const router = express.Router();
+
+// conecta ao banco
+mongoose.set('useCreateIndex', true);
+mongoose
+	.connect(
+		"mongodb+srv://hhesedh:live0491@cluster0-mudhj.mongodb.net/test?retryWrites=true",
+		{ useNewUrlParser: true }
+	)
+	.catch(err => {
+		console.log(err);
+	});
+
+// Carrega os Models
+const Product = require("./models/product");
+
+// Carrega Rotas
+const indexRoute = require("./routes/index-route");
+const productRoute = require("./routes/product-route");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan("dev"));
+
+app.use("/", indexRoute);
+app.use("/products", productRoute);
+
+module.exports = app;
