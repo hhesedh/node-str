289439797cec48698ed875f354915a1623b2aca7
Unknown <oliveira_fabio@id.uff.br>
2019-03-14 21:16:53 -0300

    implementação de jwt e autenticação de usuarios

--- src/repositories/customer-repository.js
+++ src/repositories/customer-repository.js
@@ -7,3 +7,13 @@ exports.create = async (data) => {
   var customer = new Customer(data);
   await customer.save();
 }
+
+exports.authenticate = async (data) => {
+  console.log('data', data);
+  const res = await Customer.findOne({
+    email: data.email,
+    password: data.password
+  });
+
+  return res;
+}
