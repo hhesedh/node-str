e6d1087f57ee1725e611d571e1f51bacb648c307
Unknown <oliveira_fabio@id.uff.br>
2019-03-15 23:31:04 -0300

    Implementação de regras de autenticação

--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -6,12 +6,12 @@ const controller = require("../controllers/product-controller");
 const authService = require("../services/auth-service");
 
 
-router.get("/", authService.authorize, controller.get);
+router.get("/", controller.get);
 router.get("/:slug", controller.getBySlug);
 router.get("/admin/:id", controller.getById);
 router.get("/tags/:tag", controller.getByTag);
-router.post("/", authService.authorize ,controller.post);
-router.put("/:id", authService.authorize, controller.put);
-router.delete("/", authService.authorize, controller.delete);
+router.post("/", authService.isAdmin ,controller.post);
+router.put("/:id", authService.isAdmin, controller.put);
+router.delete("/", authService.isAdmin, controller.delete);
 
 module.exports = router;
